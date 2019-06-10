369d707a305c07d0246cbf31bac05b931043d7bf
Leslie Shea <leslieyshea@gmail.com>
2019-06-10 11:19:54 -0700

    created container for CreateNote

---
+++ src/containers/notes/CreateNote.js
@@ -0,0 +1,53 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import NoteForm from '../../components/notes/NoteForm';
+import { newNote } from '../../actions/notesActions';
+
+class CreateNote extends PureComponent {
+  static propTypes = {
+    createNote: PropTypes.func.isRequired
+  }
+
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { title, body } = this.state;
+    this.props.createNote({ title, body });
+    this.setState({ title: '', body: '' });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { title, body } = this.state;
+
+    return (
+      <NoteForm
+        onSubmit={this.onSubmit}
+        onChange={this.onChange}
+        title={title}
+        body={body}
+        submitText="Create Note"
+      />
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  createNote(note) {
+    dispatch(newNote(note));
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateNote);
