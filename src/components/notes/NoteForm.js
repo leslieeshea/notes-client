658fb9feb4c85709b7721aa13dd102af28c27378
Leslie Shea <leslieyshea@gmail.com>
2019-06-10 11:11:44 -0700

    created the note form

---
+++ src/components/notes/NoteForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NoteForm({ onSubmit, onChange, title, body, submitText }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <p>Note Title: </p>
+      <input type="text" value={title} onChange={onChange} />
+
+      <p>Note Body: </p>
+      <input type="text" value={body} onChange={onChange} />
+
+      <button>{submitText}</button>
+    </form>
+  );
+}
+
+NoteForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  submitText: PropTypes.string
+};
+
+NoteForm.defaultProps = {
+  submitText: 'Submit'
+};
+
+export default NoteForm;
